d2513c2a4909e90c9dda7d653f555a7fcdc6794e
HDYS-TTBYS <tatehide151@outlook.jp>
2022-09-08 21:08:46 +0900

    コメント送信機能追加

--- app/src/firebase.ts
+++ app/src/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,6 +16,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-export { firebase };
+const database = getDatabase(app);
